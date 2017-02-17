3194509f7247b0c6b00ff0113b0ae9cda852c8e4
Dominic Ginger <domginger@hotmail.co.uk>
2017-02-17 13:32:46 +0000

    List filter implementation, could be improved. No need for the res param, just return the last item in the array when there is only one left

---
+++ my_list.ex
@@ -0,0 +1,8 @@
+defmodule MyList do
+  def flatten(_, res \\ [])
+  def flatten(last, res), do: res
+  def flatten([h|t], res) when is_list(h) do
+    flatten(h, res) ++ flatten(t, [])
+  end
+  def flatten([h|t], res), do: flatten(t, res ++ [h])
+end
