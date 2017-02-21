c93a66a1fb239b4bc2c531455831048bc8a54644
Dominic Ginger <domginger@hotmail.co.uk>
2017-02-21 23:44:02 +0000

    span function on list

--- my_list.ex
+++ my_list.ex
@@ -1,4 +1,7 @@
 defmodule MyList do
+  def span(from, to) when from > to, do: []
+  def span(from, to), do: [from|span(from+1, to)]
+
   def flatten(_, res \\ [])
   def flatten([], res), do: res
   def flatten([h|t], res) when is_list(h) do
