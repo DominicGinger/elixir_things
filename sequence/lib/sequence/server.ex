bb840f88801ae65d1c1d7dbd3e52385bca9d4dfe
Dominic Ginger <domginger@hotmail.co.uk>
2017-03-15 13:11:36 +0000

    Rewrite sequence server to be clean

--- sequence/lib/sequence/server.ex
+++ sequence/lib/sequence/server.ex
@@ -1,19 +1,27 @@
 defmodule Sequence.Server do
   use GenServer
 
-  def handle_call(:next_number, _from, current_number) do
+  def start_link(current_number) do
+    GenServer.start_link(__MODULE__, current_number, name: __MODULE__)
+  end
+
+  def next_number do
+    GenServer.call __MODULE__, :next_number
+  end
+
+  def increment_number(delta) do
+    GenServer.cast __MODULE__, {:increment_number, delta }
+  end
+
+  def handle_call(:next_number, _form, current_number) do
     { :reply, current_number, current_number + 1 }
   end
 
-  def handle_call(:pop, _from, [head|stack]) do
-    { :reply, head, stack }
-  end
-
-  def handle_cast({:increment_number, delta}, current_number) do
+  def handle_cast({ :increment_number, delta }, current_number) do
     { :noreply, current_number + delta }
   end
 
-  def handle_cast({:push, val}, stack) do
-    { :noreply, [val|stack] }
+  def format_status(_reason, [ _pdict, state]) do
+    [ data: [{ 'State', "My current state is '#{inspect state}', and I'm happy"}]]
   end
 end
